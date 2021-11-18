51fb4dd8f54372dec1352071a9285bc2dfc5d45a
BenLouWong <72722548+BenLouWong@users.noreply.github.com>
2021-11-19 09:27:25 +1100

    Update 17
    Changed list descriptions from simple icon list to grid
    Changed header to be more grabbing

--- js/renderOurList.js
+++ js/renderOurList.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const ourListDivider = document.querySelector(".listDivider");
+const ourListDivider = document.querySelector(".listGrid");
 let ourHtml;
 
 console.log(stores);
@@ -8,27 +8,32 @@ console.log(stores);
 const renderList = function () {
 	stores.features.forEach((i) => {
 		console.log(i.properties.title);
-		ourHtml = `<div class="favCard flex flex__ai-center">
-    <div class="favCard__img">
-            <img src="images/${i.properties.title}.jpg"
-            alt="card image" class="favCard__img" />
-    </div>
-
-    <div
-        class="
-            favCard__textCont
-            flex flex__ai-center flex__jc-center flex__dir-col
-        "
-    >
-        <div class="h3 heading-3 favCard__textCont--heading">
-            ${i.properties.title}
+		ourHtml = `<div class="listCard flex flex__dir-col flex__jc-sb">
+        <div class="listCard__photo">
+            <img
+                src="images/${i.properties.title}.jpg"
+                alt=""
+            />
         </div>
-        <button class="favCard__btn">
-            <h4 class="heading-4">Read More</h4>
-        </button>
-    </div>
-</div>`;
-		ourListDivider.insertAdjacentHTML("afterend", ourHtml);
+        <div class="listCard__content">
+            <div class="heading- favCard__textCont--heading">
+            ${i.properties.title}
+            </div>
+            <h3
+                class="
+                    heading-3
+                    description__heading
+                    description__heading--02
+                "
+            >
+            ${i.properties.address}, ${i.properties.city}, ${i.properties.state}, ${i.properties.postalCode}
+            </h3>
+            <button class="btn btn__01">
+                <h4 class="heading-4">Read More</h4>
+            </button>
+        </div>
+    </div>`;
+		ourListDivider.insertAdjacentHTML("afterbegin", ourHtml);
 	});
 };
 
