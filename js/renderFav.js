cb70d20ce7fde2a5c2b2433200051c096d740cb6
BenLouWong <72722548+BenLouWong@users.noreply.github.com>
2021-12-02 16:48:17 +1100

    Update 26
    Added remove favourites button

--- js/renderFav.js
+++ js/renderFav.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const listFav = JSON.parse(localStorage.getItem("cafe"));
+let listFav = JSON.parse(localStorage.getItem("cafe"));
 const favDivider = document.querySelector(".favDivider");
 let listHtml;
 const description = document.querySelector(".description");
@@ -14,26 +14,47 @@ console.log(listFav);
 
 const renderFav = function () {
 	listFav.forEach((i) => {
-		listHtml = `<div class="favCard flex flex__ai-center">
-        <div class="favCard__img">
-            <img src="images/${i.title}.jpg"
-            alt="card image" class="favCard__img" />
-        </div>
+		listHtml = `
+                    <div class="favCard flex flex__jc-sb flex__ai-center">
+					<div
+						class="
+							favCard__wrapper
+							flex flex__ai-center flex__jc-sb
+						"
+					>
+						<div class="favCard__img">
+                        <img src="images/${i.title}.jpg"
+                        alt="card image" class="favCard__img" />						</div>
 
-        <div
-            class="
-                favCard__textCont
-                flex flex__ai-center flex__jc-center flex__dir-col
-            "
-        >
-            <div class="h3 heading-3 favCard__textCont--heading">
-                ${i.title}
-            </div>
-            <button class="favCard__btn">
-                <h4 class="heading-4">Read More</h4>
-            </button>
-        </div>
-    </div>`;
+						<div
+							class="
+								favCard__textCont
+								flex
+								flex__ai-center
+								flex__jc-center
+								flex__dir-col
+							"
+						>
+                        <div class="h3 heading-3 favCard__textCont--heading">
+                        ${i.title}
+                    </div>
+							<button class="favCard__btn">
+								<h4 class="heading-4">Read More</h4>
+							</button>
+						</div>
+					</div>
+					<button class="favCard__remove" id = "remove">
+						<ion-icon name="ellipsis-horizontal"></ion-icon>
+					</button>
+					<div class="favCard__remove--cont">
+						<button
+							class="favCard__remove favCard__remove--menu"
+							id="remove02"
+						>
+							Remove
+						</button>
+					</div>
+				</div>`;
 		favDivider.insertAdjacentHTML("afterend", listHtml);
 		document
 			.querySelector(".favCard__btn")
@@ -63,12 +84,30 @@ const renderFav = function () {
 					"description__overlay--active"
 				);
 			});
+
+		const removeFav = document.querySelector(".favCard__remove");
+		const removeFav02 = document.querySelector(".favCard__remove--cont");
+
+		removeFav.addEventListener("click", function () {
+			removeFav02.classList.toggle("favCard__remove--cont-active");
+		});
+
+		const favCard = document.querySelector(".favCard");
+
+		removeFav02.addEventListener("click", function () {
+			favCard.classList.add("favCard__hidden");
+			listFav.splice(i, 1);
+			// listFav = JSON.stringify(listFav);
+			console.log(listFav);
+			localStorage.setItem("cafe", JSON.stringify(listFav));
+		});
 	});
 
 	descriptionClose.addEventListener("click", () => {
 		description.classList.remove("description__active");
 		descriptionOverlay.classList.remove("description__overlay--active");
 	});
+
 	// Description - image swiper
 };
 renderFav();
